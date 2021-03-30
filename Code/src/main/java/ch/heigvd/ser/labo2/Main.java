0f3eda5b719ba71bda22e1e820b3134558435180
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-03-30 17:45:28 +0200

    Ajout de commentaires

--- Code/src/main/java/ch/heigvd/ser/labo2/Main.java
+++ Code/src/main/java/ch/heigvd/ser/labo2/Main.java
@@ -9,7 +9,6 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 
 import java.io.*;
-import java.lang.reflect.Type;
 import java.util.List;
 
 import org.jdom2.JDOMException;
@@ -58,12 +57,12 @@ class Main
       // Pour chaque tournoi
       for (Element tournoi : tournois)
       {
-         int noPartie = 1;
+         int gameNumber = 1;
 
          // Pour chaque partie du tournoi.
          for (Element partie : tournoi.getChild("parties").getChildren())
          {
-            String resultFilename = tournoi.getAttributeValue("nom") + "_" + (noPartie++) + ".PGN";
+            String resultFilename = tournoi.getAttributeValue("nom") + "_" + (gameNumber++) + ".PGN";
 
             // Création du fichier de sortie.
             PrintWriter out = null;
@@ -78,27 +77,20 @@ class Main
             }
 
             int turn = 1;
-            boolean shouldTurnChange = false;
+            boolean blackTurn = false;
 
             // Pour chaque coup
             for (Element coup : partie.getChild("coups").getChildren())
             {
                // Création du coup spécial
-               CoupSpecial cs = getCoupspecialFromString(coup.getAttributeValue("coup_special"));
+               CoupSpecial cs = getCoupSpecialFromString(coup.getAttributeValue("coup_special"));
 
                // Est-ce un déplacement ou un roque ?
                Element typeCoup = coup.getChild("deplacement");
                Coup objCoup = null;
                if (typeCoup != null) // Le coup est un déplacement
                {
-                  try
-                  {
-                     objCoup = getDeplacementFromElement(typeCoup, cs);
-                  }
-                  catch (Exception e)
-                  {
-                     System.out.println(e.getMessage());
-                  }
+                  objCoup = getDeplacementFromElement(typeCoup, cs);
                }
                else // C'est donc un roque
                {
@@ -106,19 +98,22 @@ class Main
                   objCoup = getRoqueFromElement(typeCoup, cs);
                }
 
-               String notationPGN = objCoup.notationPGN();
+
+               String notationPGN = objCoup != null ? objCoup.notationPGN() : "";
 
 
-               if (shouldTurnChange) // Coup noir
+               if (blackTurn) // Coup noir
                {
                   out.println(" " + notationPGN);
-                  shouldTurnChange = false;
+
+                  blackTurn = false;
                   ++turn;
                }
                else // Coup blanc
                {
                   out.print(turn + " " + notationPGN);
-                  shouldTurnChange = true;
+
+                  blackTurn = true;
                }
             }
 
@@ -127,13 +122,27 @@ class Main
       }
    }
 
-   private static CoupSpecial getCoupspecialFromString(String readValue)
+   /**
+    * Crée un coup spécial en fonction de readValue.
+    * @param readValue Chaîne de caractère à convertir (sera convertie en majuscule).
+    * @return Retourne le CoupSpecial correspondant a readValue sinon null.
+    * @throws IllegalArgumentException si readValue ne correspond à aucune
+    *        valeur de CoupSpecial.
+    */
+   private static CoupSpecial getCoupSpecialFromString(String readValue)
    {
       if (readValue == null) return null;
 
       return CoupSpecial.valueOf(readValue.toUpperCase());
    }
 
+   /**
+    * Crée une pièce en fonction de readValue.
+    * @param readValue Chaîne de caractère à convertir (sera convertie en majuscule).
+    * @return Retourne la pièce correspondant a readValue sinon null.
+    * @throws IllegalArgumentException si readValue ne correspond à aucune
+    *        valeur de CoupSpecial.
+    */
    private static TypePiece getTypePieceFromString(String readValue)
    {
       if (readValue == null) return null;
@@ -141,6 +150,14 @@ class Main
       return TypePiece.valueOf(readValue);
    }
 
+   /**
+    * Retourne le type de roque de readValue
+    * @param readValue readValue Chaîne de caractère à convertir.
+    * @return Retourne un petit roque si la chaine commence par "petit" ou
+    *         un grand groque si elle commance par "grand" (non sensible à la casse).
+    * @throws IllegalArgumentException si readValue ne correspond à aucune
+    *         valeur de CoupSpecial.
+    */
    private static TypeRoque getTypeRoqueFromString(String readValue)
    {
       if (readValue == null) return null;
@@ -148,25 +165,52 @@ class Main
       return TypeRoque.valueOf(readValue.substring(0,5).toUpperCase());
    }
 
+   /**
+    * Transforme la lettre (premier caractère) en colonne et la seconde lettre en no de ligne.
+    * @param readValue Ch Chaîne à convertir.
+    * @return Retourne la case correspondante.
+    * @throws StringIndexOutOfBoundsException si le string ne fait pas au moins 2 caractères
+    * @throws NumberFormatException Si le second caractère n'est pas un entier.
+    *
+    */
    private static Case getCaseFromString(String readValue)
    {
       if (readValue == null) return null;
 
-      //readValue.charAt(1)
       return new Case(readValue.charAt(0), Integer.parseInt(readValue.substring(1,2)));
    }
 
-   private static Deplacement getDeplacementFromElement(Element elemDeplacement, CoupSpecial isSpecial) throws Exception
+   /**
+    * Retourne le déplacement correspondant à eleDeplacement (déplacement parsé) et isSpecial.
+    * @param elemDeplacement Element (deplacement) parsé.
+    * @param isSpecial Type de coup spécial associé au déplacement.
+    * @return Retourne l'objet deplacement sinon null en cas d'échec de construction.
+    */
+   private static Deplacement getDeplacementFromElement(Element elemDeplacement, CoupSpecial isSpecial)
    {
-      TypePiece movedPiece = getTypePieceFromString(elemDeplacement.getAttributeValue("piece"));
-      TypePiece killedPiece = getTypePieceFromString(elemDeplacement.getAttributeValue("elimination"));
-      TypePiece promotedTo = getTypePieceFromString(elemDeplacement.getAttributeValue("promotion"));
-      Case depart = getCaseFromString(elemDeplacement.getAttributeValue("case_depart"));
-      Case arrivee = getCaseFromString(elemDeplacement.getAttributeValue("case_arrivee"));
+      try
+      {
+         TypePiece movedPiece = getTypePieceFromString(elemDeplacement.getAttributeValue("piece"));
+         TypePiece killedPiece = getTypePieceFromString(elemDeplacement.getAttributeValue("elimination"));
+         TypePiece promotedTo = getTypePieceFromString(elemDeplacement.getAttributeValue("promotion"));
+         Case depart = getCaseFromString(elemDeplacement.getAttributeValue("case_depart"));
+         Case arrivee = getCaseFromString(elemDeplacement.getAttributeValue("case_arrivee"));
 
-      return new Deplacement(movedPiece, killedPiece, promotedTo, isSpecial, depart, arrivee);
+         return new Deplacement(movedPiece, killedPiece, promotedTo, isSpecial, depart, arrivee);
+      }
+      catch(Exception e)
+      {
+         System.out.println("Oops: " + e.getMessage());
+         return null;
+      }
    }
 
+   /**
+    * Crée l'objet roque correspondant à l'élément parsé et au coup spécial.
+    * @param elemRoque Element (roque) parsé.
+    * @param isSpecial Le type spécial du coup associé.
+    * @return Retourne l'objet roque associé.
+    */
    private static Roque getRoqueFromElement(Element elemRoque, CoupSpecial isSpecial)
    {
       TypeRoque tr = getTypeRoqueFromString(elemRoque.getAttributeValue("type"));
